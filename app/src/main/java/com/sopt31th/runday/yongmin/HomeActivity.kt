473418177e023abac1f356bf2e9e5708005b233d
brian <briandr@naver.com>
2022-11-23 23:41:04 +0900

    feat(HomeActivity): Home Activity 추가

---
+++ app/src/main/java/com/sopt31th/runday/yongmin/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt31th.runday.yongmin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt31th.runday.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
