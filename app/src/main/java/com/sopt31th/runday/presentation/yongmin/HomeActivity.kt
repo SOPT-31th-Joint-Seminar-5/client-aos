5b97121a867dbe5b3b13b715414d25ec83a9fb91
giovannijunseokim <kingjunseo@gmail.com>
2022-11-25 14:47:53 +0900

    [feat] 홈 액티비티와 메뉴 액티비티 연결

--- app/src/main/java/com/sopt31th/runday/presentation/yongmin/HomeActivity.kt
+++ app/src/main/java/com/sopt31th/runday/presentation/yongmin/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.sopt31th.runday.presentation.yongmin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sopt31th.runday.R
 import com.sopt31th.runday.databinding.ActivityHomeBinding
+import com.sopt31th.runday.presentation.mypage.MyPageFragment
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -11,5 +13,29 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        activateBottomNavigation()
+    }
+
+    private fun activateBottomNavigation() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.menu_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, HomeFragment())
+                        .commit()
+                    true}
+                R.id.menu_record -> {true}
+                R.id.menu_crew -> {true}
+                R.id.menu_challenge -> {true}
+                R.id.menu_my_menu -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, MyPageFragment())
+                        .commit()
+                    true
+                }
+                else -> {true}
+            }
+        }
     }
 }
